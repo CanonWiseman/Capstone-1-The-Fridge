d40d76e9e55e959b8cdb29cfb0cb381aa58e0fa4
CanonWiseman <wisemancanon@gmail.com>
2023-02-28 10:30:53 -0600

    update app

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('postgres://rsirnlkgnzigef:38584b1754dc0b5c85fc0411e5c597549fd78f243c08c3491de8a0802d60fa7d@ec2-34-194-158-176.compute-1.amazonaws.com:5432/dbh06onu1m8a7l'))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rsirnlkgnzigef:38584b1754dc0b5c85fc0411e5c597549fd78f243c08c3491de8a0802d60fa7d@ec2-34-194-158-176.compute-1.amazonaws.com:5432/dbh06onu1m8a7l'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
