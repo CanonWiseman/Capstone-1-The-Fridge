e4d89c52a08a16561c01696d3a7630461484ada1
CanonWiseman <wisemancanon@gmail.com>
2023-03-03 17:33:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+IngredientAPIKey = os.environ.get("IngredientAPIKey", "ingredient key")
+MealApiKey3 = os.environ.get("MealApiKey3", "meal  key")
 
 
 
